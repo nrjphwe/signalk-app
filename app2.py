aac490da7820a4e8139c8b13773aa58b2a579897
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2024-11-09 12:22:01 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -36,32 +36,55 @@ async def signalk_listener():
     async with websockets.connect(SIGNALK_SERVER_URL) as websocket:
         while True:
             message = await websocket.recv()
-            data = json.loads(message)
+            data = json.loads(message) #https://www.geeksforgeeks.org/json-loads-in-python/
 
             updates = []
             for update in data.get("updates", []):
                 for value in update.get("values", []):
                     path = value.get("path")
-                    if path in {
+                    # data from polar performance plugin
+                    if path in { # performance
                         'performance.maxSpeedAngle',
                         'performance.maxSpeed',
                         'performance.targetSpeed',
+                        'performance.targetAngle',
                         'performance.polarSpeed',
                         'performance.gybeAngle',
                         'performance.beatAngle',
-                        'performance.targetAngle',
-                        "environment.wind.angleApparent",
-                        "navigation.headingMagnetic",
-                        "steering.autopilot.state",
-                        'steering.autopilot.target.windAngleApparent',
-                        'navigation.headingMagnetic',
+                    }:
+                        updates.append(value)
+                    # data fromn autopilot
+                    elif path in { #steering
                         'steering.autopilot.target.headingMagnetic',
+                        'steering.autopilot.target.headingTrue', # APB
+                        'steering.autopilot.state',
+                        'steering.autopilot.target.windAngleApparent',
+                    }:
+                        updates.append(value)
+                    # other data
+                    elif path in { # navigation 
+                        'navigation.headingTrue', # pgn:127250
+                        'navigation.headingMagnetic',
+                        'navigation.magneticVariation', # RMC, HDG & pgn:127250
+                        'navigation.courseOverGroundTrue', # VTG & pgn: 129038
+                        'navigation.speedOverGround', # SOG ,VTG & pgn:129039
+                        'navigation.speedThroughWater', # VHW
+                        'navigation.courseOverGroundTrue', # pgn:129039
                     }:
                         updates.append(value)
 
+                    elif path in { # environment
+                        "environment.wind.angleApparent",
+                        'environment.wind.directionTrue',
+                        'environment.depth.belowKeel',
+                        'environment.wind.angleTrueWaterDamped',
+                        'environment.wind.speedTrue', # VWT
+
+                    }:
+                        updates.append(value)
             if updates:
                 socketio.emit("update_data", {"updates": updates})
-                #print(f"a34 Emitted data: {updates}")
+                print(f"a84 Emitted data: {updates}")
 
 # Wrapper function to start the asyncio event loop
 def run_async_task():
